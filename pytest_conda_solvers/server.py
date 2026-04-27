cf53084cb42cc33292a6c798fcfe668302b83b6d
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-04-27 19:10:17 +0530

    Work around HTTP status code a little bit

--- pytest_conda_solvers/server.py
+++ pytest_conda_solvers/server.py
@@ -66,9 +66,13 @@ def channel_server(host="localhost", port=8080):
     async def repodata(
         channel_name: str,
         subdir: str,
-        filename: RepodataFilename,
+        filename: str,
     ):
-        return get_channel_repodata(channel_name, subdir, filename.value)
+        try:
+            validated = RepodataFilename(filename)
+        except ValueError:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return get_channel_repodata(channel_name, subdir, validated.value)
 
     @app.get("/{full_path:path}")
     async def catch_all():
