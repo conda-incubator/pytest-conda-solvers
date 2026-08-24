d173045fd3717118a933a914ad7fb0494b2e25c9
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-08-25 02:26:13 +0530

    Invoke the beloved walrus
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- pytest_conda_solvers/base_tests/install.py
+++ pytest_conda_solvers/base_tests/install.py
@@ -380,8 +380,8 @@ class TestBasic:
             case ResolvePackageNotFound() as exc:
                 # classic solver only. bad_deps is a flat tuple of MatchSpecs, wrapped
                 # here to match the set-of-tuples structure in error_info["entries"]
-                if error_info.get("entries"):
-                    assert set((exc.bad_deps,)) == set(error_info["entries"])
+                if entries := error_info.get("entries"):
+                    assert set((exc.bad_deps,)) == set(entries)
             case PackagesNotFoundError() as exc:
                 if error_info.get("entries"):
                     expected_names = {
