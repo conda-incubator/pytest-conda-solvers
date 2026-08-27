faaeb209436890e2dad78468f160354b446c179c
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-08-27 06:47:32 +0530

    Add comment about `register_assert_rewrite` usage

--- pytest_conda_solvers/base_tests/__init__.py
+++ pytest_conda_solvers/base_tests/__init__.py
@@ -1,4 +1,7 @@
 import pytest
 
+# pytest only rewrites assert statements in modules it collects as tests.
+# These two files are imported as plugin code, so without this registration,
+# their asserts fail without offering us any useful detail in the output.
 pytest.register_assert_rewrite("pytest_conda_solvers.base_tests.install")
 pytest.register_assert_rewrite("pytest_conda_solvers.base_tests.solve")
